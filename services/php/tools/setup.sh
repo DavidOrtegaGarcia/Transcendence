3400c104d44364eae959fb4a38a6beafd893301e
David Ortega <daorga77@gmail.com>
2026-01-23 22:11:46 +0100

    MINOR CHANGES

--- services/php/tools/setup.sh
+++ services/php/tools/setup.sh
@@ -4,4 +4,8 @@
 composer install --no-interaction --no-scripts
 # Install databases
 php artisan migrate --force
+#Give permissions 
+chown -R www-data:www-data /var/www/storage /var/www/bootstrap/cache
+chmod -R 775 /var/www/storage /var/www/bootstrap/cache
+
 exec "$@"
