d68d9b076ff31a7e1f7ffc20b8f35eec4c8b689f
Kevin Seligmann <seligmann.kevin.cc@gmail.com>
2026-01-26 21:44:10 +0100

    Update table migrations to reflect a new version of the data model.
    After reading the requirements of the game more in detail, it has been decided to update the data model.
    
    The details of this new model are subject to business rules and should be specified its own document.

--- services/php/www/database/migrations/2026_01_04_063910_create_cards_table.php
+++ services/php/www/database/migrations/2026_01_04_063910_create_cards_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('cards', function (Blueprint $table) {
             $table->id();
+            $table->timestamps();
             $table->string('name');
             $table->string('back_image');
             $table->string('front_image');
