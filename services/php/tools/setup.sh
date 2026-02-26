3d544c108b49bb943b05c122dff516c29a1f0a32
David Ortega <daorga77@gmail.com>
2026-02-26 02:53:48 +0100

    Order of exports fixed in services/php/setup.sh

--- services/php/tools/setup.sh
+++ services/php/tools/setup.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
+#Set secret variables in env
+if [ -f /run/secrets/app_key ]; then
+    export APP_KEY=$(cat /run/secrets/app_key | tr -d '\n')
+fi
+if [ -f /run/secrets/reverb_app_secret ]; then
+    export REVERB_APP_SECRET=$(cat /run/secrets/reverb_app_secret | tr -d '\n')
+fi
+if [ -f /run/secrets/db_password ]; then
+    export DB_PASSWORD=$(cat /run/secrets/db_password | tr -d '\n')
+fi
+
 # Install dependencies
 composer install --no-interaction --no-scripts
 # Install databases
@@ -7,10 +18,5 @@ php artisan migrate --force
 #Give permissions 
 chown -R www-data:www-data /var/www/storage /var/www/bootstrap/cache
 chmod -R 775 /var/www/storage /var/www/bootstrap/cache
-
-#Set secret variables in env
-export APP_KEY=$(cat /run/secrets/app_key)
-export REVERB_APP_SECRET=$(cat /run/secrets/reverb_app_secret)
-export DB_PASSWORD=$(cat /run/secrets/db_password)
 
 exec "$@"
