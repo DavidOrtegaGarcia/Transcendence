4902d7a4725e9c970d8b440f9b494b2991462d69
MiriamFernandezPerez <mirindaww@hotmail.com>
2026-02-28 19:20:36 +0100

    Testing avatars 2

--- services/php/www/routes/api.php
+++ services/php/www/routes/api.php
@@ -5,16 +5,5 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
 Route::middleware('auth:sanctum')->group(function () {
-    // Group v1 definition
-    Route::prefix('v1')->group(function () {
-        // Route to get logged-in user data (Login)
-        Route::get('/user', [UserController::class, 'getOwnUser']);
-        
-        // Actualization routes
-        Route::patch('/user/update', [UserController::class, 'updateUser']);
-        Route::put('/user/password', [UserController::class, 'updateOwnPassword']);
-    });
-
-    // Public profiles route
-    Route::get('/users/{user}', [UserController::class, 'getUser']);
+ 
 });
